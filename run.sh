a984867bb8cb14c316c36fc0706755c2aa25c7c5
Claire Gregg <clairegregg@clairegregg.com>
2024-11-11 14:45:47 +0000

    Fix port

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ pip3 install -r requirements.txt
 
 # openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365 -nodes
 
-hypercorn src.app:app --certfile cert/cert.pem --keyfile cert/key.pem --bind 0.0.0.0:33000 --reload
+hypercorn src.app:app --certfile cert/cert.pem --keyfile cert/key.pem --bind 0.0.0.0:33001 --reload
